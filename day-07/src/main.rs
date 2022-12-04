c8b4147abc68c4fbbe2fb146c74c57b5545d7205
Guido Knips <guido.knips@posteo.de>
2022-12-04 14:07:26 +0100

    Add solution for day 7, part 2
    Kind of clumsy solution, but it works.

--- day-07/src/main.rs
+++ day-07/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 use std::env;
 use std::fs::read_to_string;
 use std::path::Path;
@@ -15,9 +15,16 @@ fn main() -> Result<(), String> {
     })?;
     println!("The root program is '{root_name}'");
 
+    if let Some(corrected_weight) = find_corrected_weight(&progs, root_name) {
+        println!("The corrected weight is {corrected_weight}");
+    } else {
+        println!("Either there is no weight to be corrected or something is wrong with the tree datastcutures");
+    }
+
     Ok(())
 }
 
+#[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Debug)]
 struct Prog<'a> {
     name: &'a str,
     weight: u32,
@@ -69,6 +76,67 @@ fn find_root_program<'a>(progs: &'a [Prog]) -> Option<&'a str> {
     candidates.iter().next().copied()
 }
 
+fn calculate_prog_weights<'a, 'w>(
+    progs: &'a HashMap<&'a str, &Prog>,
+    root: &'a str,
+    weights: &'w mut HashMap<&'a str, u32>,
+) -> u32 {
+    let mut weight = 0;
+    if let Some(prog) = progs.get(root) {
+        weight += prog.weight;
+        for child in &prog.children {
+            weight += calculate_prog_weights(progs, child, weights);
+        }
+        weights.insert(root, weight);
+    }
+    weight
+}
+
+fn find_corrected_weight_recursive(
+    prog: &Prog,
+    progs_by_name: &HashMap<&str, &Prog>,
+    weights: &HashMap<&str, u32>,
+) -> Option<u32> {
+    if prog.children.is_empty() {
+        return None;
+    }
+    // if a child cannot be found, something is wrong with the tree data and we won't
+    // look any further
+    let ref_prog = progs_by_name.get(prog.children[0])?;
+    let ref_weight = *weights.get(ref_prog.name)?;
+
+    let mut different: Option<(&Prog, u32)> = None;
+    for child_name in &prog.children[1..] {
+        let child_prog = progs_by_name.get(child_name)?;
+        let child_weight = weights.get(child_prog.name)?;
+
+        if let Some((oref_prog, oref_weight)) = different {
+            // Oh, btw, this will panic if the children of the node alone are heavier than
+            // the other two because then we would require a negative weight
+            return Some(if oref_weight == *child_weight {
+                // look recursively: if everything is balanced there, you have the wrong weight
+                find_corrected_weight_recursive(ref_prog, progs_by_name, weights)
+                    .unwrap_or(oref_weight - (ref_weight - ref_prog.weight))
+            } else {
+                find_corrected_weight_recursive(oref_prog, progs_by_name, weights)
+                    .unwrap_or(ref_weight - (oref_weight - oref_prog.weight))
+            });
+        } else if ref_weight != *child_weight {
+            different = Some((child_prog, *child_weight));
+        }
+    }
+    None
+}
+
+fn find_corrected_weight(progs: &[Prog], root: &str) -> Option<u32> {
+    let progs_by_name: HashMap<&str, &Prog> = progs.iter().map(|prog| (prog.name, prog)).collect();
+    let mut weights: HashMap<&str, u32> = HashMap::with_capacity(progs.len());
+    calculate_prog_weights(&progs_by_name, root, &mut weights);
+
+    let root_prog = progs_by_name.get(root)?;
+    find_corrected_weight_recursive(root_prog, &progs_by_name, &weights)
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -98,5 +166,17 @@ cntj (57)
 
         // then
         assert_eq!(root, Some("tknk"));
+    }
+
+    #[test]
+    fn find_corrected_weight_works_for_example() {
+        // given
+        let progs = parse_progs(EXAMPLE).expect("Expected successful parsing");
+
+        // when
+        let weight = find_corrected_weight(&progs, "tknk");
+
+        // then
+        assert_eq!(weight, Some(60));
     }
 }
